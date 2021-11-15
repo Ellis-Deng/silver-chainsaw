359c8f5f87dd4c6ad7bd8d4912cfce3e802e1245
Ajay <you@example.com>
2021-11-15 14:53:29 -0500

    added possible_user_input.py

---
+++ possible_user_input.py
@@ -0,0 +1,19 @@
+"""A possible way to record user input on their spendings"""
+
+
+
+class Spendings:
+    """Records a user's spendings
+    
+    
+    Attributes:
+        total (int): the total amount that a user spend in a given amount of 
+        time        
+        
+        purchases (list): the types of items that the user spent money on
+        
+        all_spending_info (dictionary): a dictionary where the keys are the 
+        date of the purchase and the values are the items bought and the 
+        price paid for them
+        
+    """
