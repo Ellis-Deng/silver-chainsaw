a6d53322c2fe5bce0f69ef89cdecb2dda1c3dede
Ellis-Deng <edeng@terpmail.umd.edu>
2021-11-15 21:50:16 -0500

    added a graph method

--- dataframe_graph.py
+++ dataframe_graph.py
@@ -1,8 +1,9 @@
 """This is a possible way to turn a CSV file into a dataframe and then create 
-a time series graph. Also, creating a new dataset for the average amount that
+a different graphs for the data. Also, creating a new dataset for the average amount that
 an individual spends on a specific item."""
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def open_file(fpath, folder):
     """Opens a CSV file and turns it into a dataframe.
@@ -22,5 +23,15 @@ def graph(purchase_data):
     """Creates a graph based off of the given data. 
 
     Args:
-        purchase_data (dataset): a dataset of all purchases 
+        purchase_data (dataset): a dataset of all purchases.
+        
+    Returns:
+        A time series graph for all purchases that have been made. 
     """
+    purchase_data.plot(kind = 'line',
+                        x = 'date',
+                        y = 'price',
+                        color = 'red')
+    plt.title('Purchases')
+    return plt.show()
+
